fd026afc7a1d65573293f61ca498cdbf5562d124
Nikk-web14 <nikita.ns321ns@gmail.com>
2025-01-12 15:03:50 +0530

    Update talwindreact.js

--- talwindreact.js
+++ talwindreact.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl bg-red-700 text-white'>
+        hello world
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
