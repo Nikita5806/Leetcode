c13621028bbbd3d6f60ba78621c4c73406c8e06a
Nikk-web14 <nikita.ns321ns@gmail.com>
2025-01-13 22:14:38 +0530

    Update reactlearn.js

--- reactlearn.js
+++ reactlearn.js
@@ -1,1 +1,17 @@
+import React from 'react'
 
+const App = () => {
+  return (
+    <div>
+      <form >
+        <input 
+        onChange={(e)=>{
+          console.log(e.target.value)
+          }}
+        type="text"  placeholder='enter you name'/>
+      </form>
+    </div>
+  )
+}
+
+export default App
